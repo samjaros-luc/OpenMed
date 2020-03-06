6a58eeb0dda59171ef0a3833f31542f3c8656941
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-03-06 14:52:15 -0500

    Update disease.py

--- disease.py
+++ disease.py
@@ -5,15 +5,8 @@ class Disease:
         self.start = start
         self.end = end
 
-    def __eq__(self, other):
-        if self.id_type == other.id_type and self.id_data != other.id_data:
-            return False
-        return self.first_name == other.first_name and self.last_name == other.last_name and self.sex == other.sex
-
-
     def get_symptoms(self):
         return self.symptoms
-
 
 #Test:
 
@@ -21,5 +14,4 @@ symptoms = ["cough", "runny nose", "sneeze"]
 disease1 = Disease("common cold", symptoms, "1/1/20", "1/5/20")
 
 print(disease1.symptoms)
-
 print(disease1.get_symptoms())
