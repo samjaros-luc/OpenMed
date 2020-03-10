8b6eb798608bcc8e32c65888b3033a019fa8479f
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-03-09 22:52:12 -0500

    Update disease.py

--- disease.py
+++ disease.py
@@ -1,17 +1,24 @@
+import datetime
+
 class Disease:
     def __init__(self, name="", symptoms=[], start="", end=""):
         self.name = name
         self.symptoms = symptoms
         self.start = start
         self.end = end
-
+    
+    def __str__(self):
+        return str(self)
+        
     def get_symptoms(self):
         return self.symptoms
 
 #Test:
 
 symptoms = ["cough", "runny nose", "sneeze"]
-disease1 = Disease("common cold", symptoms, "1/1/20", "1/5/20")
+start = datetime.date(2020, 1, 1)
+end = datetime.date(2020, 1, 5)
+disease1 = Disease("common cold", symptoms, start, end)
 
 print(disease1.symptoms)
 print(disease1.get_symptoms())
