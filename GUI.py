3ceb49e56f15d8b55c37db90fbc896fb4bda8365
Samuel Jaros <sjaros@luc.edu>
2020-03-09 22:59:10 -0500

    Icon & background picture

--- GUI.py
+++ GUI.py
@@ -1,26 +1,11 @@
 from tkinter import *
 
 window = Tk()   # create window
+window.title("OpenMed")
+window.iconphoto(True, PhotoImage(file="C:/Users/samja/OneDrive/Loyola - Semester 8/COMP 363/OpenMed/images/window_icon.png"))
 
-## Put some text in the window
-title = Label(window, text="This is my window")
-title.pack()   # put the text wherever it fits in the window
-
-## Invisible frame
-topFrame = Frame(window)
-topFrame.pack(side=TOP)   # parameter not really necessary
-bottomFrame = Frame(window)
-bottomFrame.pack(side=BOTTOM)
-
-## Make a button
-button1 = Button(topFrame, text="Button 1", fg="red")
-button2 = Button(topFrame, text="Button 2", fg="blue")
-button3 = Button(topFrame, text="Button 3", fg="green")
-button4 = Button(bottomFrame, text="Button 4", fg="purple")
-
-button1.pack(side=LEFT)
-button2.pack(side=LEFT)
-button3.pack(side=LEFT)
-button4.pack(side=BOTTOM)
+background_image = PhotoImage(file="C:/Users/samja/OneDrive/Loyola - Semester 8/COMP 363/OpenMed/images/rushing_ambulance.png")
+background_label = Label(window, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 window.mainloop()
