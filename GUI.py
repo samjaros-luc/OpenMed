74ed23fcc90eabf2268d92dcc573536fe1b33a57
Samuel Jaros <sjaros@luc.edu>
2020-03-19 21:31:10 -0500

    Working on login

--- GUI.py
+++ GUI.py
@@ -1,12 +1,16 @@
 from tkinter import *
+from PIL import ImageTk,Image
+
+primary_color = ""
+background_color = "#f0c5c5"
 
 window = Tk()   # create window
 window.title("OpenMed")
-window.iconphoto(True, PhotoImage(file="C:/Users/samja/OneDrive/Loyola - Semester 8/COMP 363/OpenMed/images/window_icon.png"))
+window.iconbitmap("images/window_icon.ico")
 
-background_image = PhotoImage(file="C:/Users/samja/OneDrive/Loyola - Semester 8/COMP 363/OpenMed/images/rushing_ambulance.png")
+background_image = ImageTk.PhotoImage(Image.open("images/rushing_ambulance.png"))
 background_label = Label(window, image=background_image)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
+background_label.pack()
 
 ## Draw login area
 def submitLogin():
@@ -19,18 +23,21 @@ def submitLogin():
     # TODO verify credentials
     # go to next page
 
-login_frame = Frame(window, bg="#f0c5c5")
+login_frame = Frame(window, padx=100, pady= 150, bg=background_color)
 login_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
+program_name = Label(login_frame, text="OpenMed", fg="white", font=("Veranda", 36), pady=10)
+program_name.grid(row=0, column=0)
+
 username_field = Entry(login_frame)
-username_field.pack()
+username_field.grid(row=1,column=0)
 username_field.insert(0, "Username")
 
 password_field = Entry(login_frame)
-password_field.pack()
+password_field.grid(row=2,column=0)
 password_field.insert(0, "Password")
 
 submit_button = Button(login_frame, text="Login")
-submit_button.pack()
+submit_button.grid(row=3,column=0)
 
 window.mainloop()
