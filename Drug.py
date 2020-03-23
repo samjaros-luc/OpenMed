ac7443cd9f424783354c61c69d714ced980a6301
Henry Wittich <hwittich@luc.edu>
2020-03-22 22:00:56 -0500

    added __str__ method and implemented datetime

--- Drug.py
+++ Drug.py
@@ -1,10 +1,10 @@
 #add start and end dates (implement datetime class)
 #add mutator functions for changing start and end date of a Drug
-#either remove side_effects and incompatbialbe drugs and implement an API or leave them
 
+import datetime
 class Drug:
     #initializer
-    def __init__(self, name="", generic_name="",dosage="",diseases=[],side_effects=[],incompatible_drugs=[]):
+    def __init__(self, name="", generic_name="",dosage="",diseases=[],side_effects=[],incompatible_drugs=[],start=None,end=None):
         self.name = name
         self.generic_name = generic_name
         #Currently using dosage as a string so that we can include units.
@@ -13,8 +13,8 @@ class Drug:
         self.diseases = diseases
         self.side_effects = side_effects
         self.incompatible_drugs = incompatible_drugs
-        #We previously talked about including a start and end date for a drug,
-        #do we feel this is covered in the Medical_Event class?
+        self.start = start
+        self.end = end
 
     #accessors
     def get_name(self):
@@ -33,6 +33,36 @@ class Drug:
         return self.side_effects
 
     #mutators
+    def set_end_date(self,end=None):
+        self.end = end
 
     #equals
-    def __eq__(self,other):
+    def __str__(self):
+        string = self.name+'; '+self.generic_name+'; '+self.dosage+'; '
+        diseases = ''
+        for disease in self.diseases:
+            diseases = diseases + disease +', '
+        string = string + diseases +'; '
+        side_effects = ''
+        for side_effect in self.side_effects:
+            side_effects = side_effects + side_effect +', '
+        string = string + side_effects +'; '
+        incompatible_drugs = ''
+        for other_drug in self.incompatible_drugs:
+            incompatible_drugs = incompatible_drugs + other_drug+', '
+        string = string + incompatible_drugs + '; '
+        string = string +'Start: '+str(self.start)+'; End: '+str(self.end)
+        return string
+
+#Testing
+start1 = datetime.date(2000,2,29)
+end1 = datetime.date(2020,2,29)
+diseases1 = ['allergies']
+side_effects1 = ['headache','sleepiness','fatigue','nervousness','stomach pain','diarrhea','dry mouth','sore throat','hoarseness']
+drug1 = Drug('Claritin','Ioratadine','10 mg',diseases1,side_effects1,[],start1,end1)
+print(drug1)
+
+diseases2 = ['acne']
+side_effects2 = ['dryness','mood changes','stomach pain','nausea']
+drug2 = Drug('Accutane','Isotretinoin','1.0 mg',diseases2,side_effects2,[],start1,end1)
+print(drug2)
