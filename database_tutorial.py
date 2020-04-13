72a7641bfe05697384d5cd170403a3592be6c4e3
Samuel Jaros <sjaros@luc.edu>
2020-04-13 16:22:03 -0500

    Update tutorial

--- database_tutorial.py
+++ database_tutorial.py
@@ -2,6 +2,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+### Make app to interact with auth & firestore ###
+# Documentation for client : https://googleapis.dev/python/firestore/latest/client.html
+# Documentation for firebase_admin : https://firebase.google.com/docs/reference/admin/python
 # Get creditentails using token from service account (.json file, Sam can send it to you)
 cred = credentials.Certificate("C:/Users/samja/OneDrive/Loyola - Semester 8/COMP 363/OpenMed/openmed-comp363-firebase-adminsdk-qglti-767b82d10f.json")
 # Create instance of application
@@ -13,6 +16,7 @@ app = firebase_admin.initialize_app(cred)
 
 ### Interact with firestore ###
 # Create client for interacting with firestore
+# Documentation : https://googleapis.dev/python/firestore/latest/document.html
 client = firestore.client(app=app)
 
 documentRef = client.document("medical_events/example")  # Get the Document Reference using the filepath (string)
