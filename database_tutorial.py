282069bf3c3fd698406b9f1c6c62d2c3d68cfa7b
Samuel Jaros <sjaros@luc.edu>
2020-04-13 16:10:34 -0500

    Add database_tutorial.py

---
+++ database_tutorial.py
@@ -0,0 +1,23 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Get creditentails using token from service account (.json file, Sam can send it to you)
+cred = credentials.Certificate("C:/Users/samja/OneDrive/Loyola - Semester 8/COMP 363/OpenMed/openmed-comp363-firebase-adminsdk-qglti-767b82d10f.json")
+# Create instance of application
+app = firebase_admin.initialize_app(cred)
+
+
+### Interact with auth ###
+
+
+### Interact with firestore ###
+# Create client for interacting with firestore
+client = firestore.client(app=app)
+
+documentRef = client.document("medical_events/example")  # Get the Document Reference using the filepath (string)
+documentSnap = documentRef.get()   # .get() gets a Document Snapshot
+dictionary_of_Data = documentSnap.to_dict()   # .to_dict() turns the document data into a python dictionary
+does_document_exist = documentSnap.exists   # boolean whether or not the document exists
+document_id = documentSnap.id   # string of document's ID
+specific_field_ICD10 = documentSnap.get("ICD10")   # rather than getting a whole dictionary, you can also just get a specific field
